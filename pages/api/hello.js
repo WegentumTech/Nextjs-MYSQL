0b89cda310e52751b0aa72e49a2f5ea2f3781a12
WegentumTech <nitesh@wegentumtech.com>
2022-09-29 12:33:17 +0530

    post query in MySql

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,46 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mysql from "mysql2"
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+
+    // Server: sql6.freemysqlhosting.net
+    // Name: sql6522284
+    // Username: sql6522284
+    // Password: WKfhZYZxRc
+    // Port number: 3306
+    const dbConnection = mysql.createConnection({
+        host: 'sql6.freemysqlhosting.net',
+        user: 'sql6522284',
+        database: 'sql6522284',
+        password: 'WKfhZYZxRc',
+        port: '3306'
+    });
+
+
+    dbConnection.query(
+        "INSERT INTO NahiBatayenge (id, Name, Address, Pata, Naya) VALUES ('1', 'Nitesh',' Mobile', 'Computer',' Hat')",
+        function(err, results, fields) {
+            console.log(results);
+
+
+            res.status(200).json(results)
+            console.log(err)
+
+        }
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
